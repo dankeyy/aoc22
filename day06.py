9c00aef70760d0b03f0b91527292c8c405dadf5b
dankeyy <dankeyy@protonmail.com>
2022-12-06 14:41:24 +0200

    always run after var rename

--- day06.py
+++ day06.py
@@ -1,5 +1,5 @@
 def solve(packet: str, w: int) -> int:
-    return next(i for i in range(w, len(p)) if len({*p[i - w: i]}) == w)
+    return next(i for i in range(w, len(packet)) if len({*packet[i - w: i]}) == w)
 
 packet = open("06.txt").read()
 print(solve(packet, 4))  # p1
