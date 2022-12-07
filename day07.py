c753a6adf815fedca7ed9d99fed0ec0ec6f8b06e
dankeyy <dankeyy@protonmail.com>
2022-12-07 19:22:59 +0200

    fix backwards comments

--- day07.py
+++ day07.py
@@ -1,23 +1,22 @@
 cwd  = []
 seen = []
-_, *data = map(str.splitlines, open("07.txt").read().split("$ "))
+_, *data = map(str.splitlines, open("07test.txt").read().split("$ "))
 
 for instruction in data:
     command, *rest = instruction
 
-    if rest: # cd, could also just check eq on command
+    if rest: # ls
         for f in rest:
             n, _ = f.split() # e.g 29116 f or `dir bla` (file/ dir names don't matter)
             if n != 'dir':
                 for i in range(len(cwd)):
                     cwd[i] += int(n)
-    else: # ls
+    else: # cd, could also just check eq on command
         _, dest = command.split() # cd / -> dest='/'
         if dest == '..':
             seen.append(cwd.pop())
         else:
             cwd.append(0)
-
 seen.extend(reversed(cwd))
 
 print(sum(x for x in seen if x <= 100000)) # p1
