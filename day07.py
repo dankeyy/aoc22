78da6fcd36357954148ec814e1a1f26bec4f6b8b
dankeyy <dankeyy@protonmail.com>
2022-12-07 19:05:45 +0200

    done part 2

--- day07.py
+++ day07.py
@@ -1,11 +1,11 @@
 cwd  = []
 seen = []
-_, *data = map(str.splitlines, open("07test.txt").read().split("$ "))
+_, *data = map(str.splitlines, open("07.txt").read().split("$ "))
 
 for instruction in data:
     command, *rest = instruction
 
-    if not rest: # cd
+    if not rest: # cd, could also just check eq on command
         _, dest = command.split() # cd / -> dest='/'
         if dest == '..':
             v = cwd.pop()
@@ -14,11 +14,16 @@ for instruction in data:
             cwd.append(0)
     else: # ls
         for f in rest:
-            a, b = f.split() # e.g 29116 f
+            a, b = f.split() # e.g 29116 f or `dir bla` (file/ dir names don't matter)
             if a != 'dir':
                 for i in range(len(cwd)):
                     cwd[i] += int(a)
 
+for _ in range(len(cwd)):
+    seen.append(cwd.pop())
 
-print(seen)
-print(sum(x for x in seen if x <= 100000))
+print(sum(x for x in seen if x <= 100000)) # p1
+#                total    - outermost
+leftover_space = 70000000 - seen[-1]
+needed_space = 30000000 - leftover_space
+print(next(x for x in seen if x >= needed_space)) # p2
