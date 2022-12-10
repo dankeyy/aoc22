7c8454fb32dd917099a7d4317e0c4d7d7de51c62
dankeyy <dankeyy@protonmail.com>
2022-12-10 16:59:06 +0200

    cleanup

--- day10.py
+++ day10.py
@@ -1,19 +1,19 @@
 lines = (l.partition(' ') for l in reversed(open('10.txt').read().splitlines()))
 ops = [int(n or 0) for _op, _, n in lines]
-strengh = crt_pos = intbuf = 0
+strengh = intbuf = 0
 cycles = x = 1
 crt = ['.'] * 40
 
 while ops:
-    if (cycles - 20) % 40 == 0:
+    if (cycles - 20) % 40 == 0: # part 1 cycles, 20 and up, steps of 40
         strengh += cycles * x
 
-    elif cycles % 40 == 0:
+    elif cycles % 40 == 0:      # wipe & rewrite crt line
         print(*crt, sep='')
         crt = ['.'] * 40
 
     pos = cycles % 40 -1
-    if x - 1 <= pos <= x + 1: # in sprite
+    if x - 1 <= pos <= x + 1:   # pos in sprite
         crt[pos] = '#'
 
     if intbuf:
